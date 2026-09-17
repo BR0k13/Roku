c30a0f43c27b8ed52df2c327d873223fa69e9357
BROKIE <165856630+BR0k13@users.noreply.github.com>
2026-09-17 16:01:20 +0500

    Add AniDB source

--- javascript/anime/src/en/anidb.js
+++ javascript/anime/src/en/anidb.js
@@ -1,1 +1,24 @@
+const mangayomiSources = [
+    {
+        "name": "AniDB",
+        "lang": "en",
+        "baseUrl": "https://anidb.se",
+        "apiUrl": "",
+        "iconUrl": "https://anidb.se/favicon.ico",
+        "typeSource": "single",
+        "itemType": 1,
+        "isNsfw": false,
+        "version": "0.1.0",
+        "pkgPath": "anime/src/en/anidb.js",
+        "notes": "AniDB anime source"
+    }
+];
 
+class DefaultExtension extends MProvider {
+
+    constructor() {
+        super();
+        this.client = new Client();
+    }
+
+}
