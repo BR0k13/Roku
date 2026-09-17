07c1c3e32f5047ad675ca3ddd23086a985ca6336
BROKIE <165856630+BR0k13@users.noreply.github.com>
2026-09-17 16:33:32 +0500

    Update anidb.js

--- javascript/anime/src/en/anidb.js
+++ javascript/anime/src/en/anidb.js
@@ -8,7 +8,7 @@ const mangayomiSources = [
         "typeSource": "single",
         "itemType": 1,
         "isNsfw": false,
-        "version": "0.1.0",
+        "version": "0.1.1",
         "pkgPath": "anime/src/en/anidb.js",
         "notes": "AniDB anime source"
     }
